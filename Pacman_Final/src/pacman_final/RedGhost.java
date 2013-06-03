056e9a22781e6a733942bd5e74f481959ce803b8
mattraneri <mattraneri@b569dfcc-ab1b-386b-7d50-3827569c1f77>
2013-06-03 20:41:59 +0000

    Ghost super class is pretty much done.  The updating must be done inside the GUIPanel so it can access the grid.  =D

---
+++ Pacman_Final/src/pacman_final/RedGhost.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacman_final;
+
+/**
+ *
+ * @author matt
+ */
+public class RedGhost extends Ghost{
+    //Red images are default, not a problem- no changing of images nessesary
+    public RedGhost()
+    {
+        super();
+    }
+}
