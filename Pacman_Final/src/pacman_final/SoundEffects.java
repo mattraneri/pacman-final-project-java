d1fcd07d905e789b2eef52539d3fd6193a908ee4
mattraneri <mattraneri@b569dfcc-ab1b-386b-7d50-3827569c1f77>
2013-06-05 21:50:27 +0000

    Sound effect stuff.

--- Pacman_Final/src/pacman_final/SoundEffects.java
+++ Pacman_Final/src/pacman_final/SoundEffects.java
@@ -18,7 +18,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public class SoundEffects {
     
     static Clip alarm = null;
-    
+    static boolean openClose = false;
     public static void playEntry() {
         try {
             Clip clip = AudioSystem.getClip();
@@ -67,6 +67,49 @@ public class SoundEffects {
     public static void stopAlarm() {
         if(alarm != null)
             alarm.stop();
+    }
+    
+    public static void contactDot()
+    {
+        if(openClose == false)
+        {
+            playOpen();
+        } else {
+            playClose();
+        }
+    }
+    
+    public static void playClose()
+    {
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream input;
+            input = AudioSystem.getAudioInputStream(SoundEffects.class.getResource("/sounds/pacman_close.wav"));
+            clip.open(input);
+            clip.start();
+        } catch (LineUnavailableException e) {
+            System.out.println(e.getMessage());
+        } catch (UnsupportedAudioFileException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void playOpen() {
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream input;
+            input = AudioSystem.getAudioInputStream(SoundEffects.class.getResource("/sounds/pacman_open.wav"));
+            clip.open(input);
+            clip.start();
+        } catch (LineUnavailableException e) {
+            System.out.println(e.getMessage());
+        } catch (UnsupportedAudioFileException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
     
     public static void playPacmanDead()
