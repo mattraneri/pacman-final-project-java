cc6fd04a8b8a4d7c93122a70784671b014a88460
Habstinat <Habstinat@gmail.com>
2013-06-09 01:25:08 +0000

    Made final as per <http://www.reddit.com/r/java/comments/1fy7nu/we_made_a_pacman_clone_in_java_for_our_ap/caeyzwz> (thanks again intrepidengineer!)

--- Pacman_Final/src/pacman_final/Direction.java
+++ Pacman_Final/src/pacman_final/Direction.java
@@ -9,8 +9,8 @@ package pacman_final;
  * @author matt
  */
 public class Direction {
-    public static int UP = 0;
-    public static int DOWN = 1;
-    public static int LEFT = 2;
-    public static int RIGHT = 3;
+    public static final int UP = 0;
+    public static final int DOWN = 1;
+    public static final int LEFT = 2;
+    public static final int RIGHT = 3;
 }
