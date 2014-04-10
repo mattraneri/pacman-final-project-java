707e2b36f37f2659821ecc9138e75a15f672fb7e
mattraneri <mattraneri@b569dfcc-ab1b-386b-7d50-3827569c1f77>
2014-04-10 21:41:24 +0000

    Felt like making the code more tidy and fixing some issues we were previously having.  The alarm sound was broken and wouldn't continue on a loop, a problem that is now fixed.

--- Pacman_Final/src/pacman_final/SoundEffects.java
+++ Pacman_Final/src/pacman_final/SoundEffects.java
@@ -17,61 +17,90 @@ import java.net.URL;
  * @author matt
  */
 public class SoundEffects {
-    
-    static Clip alarm = null;
+    static boolean alarmPlay = true;
     static boolean openClose = false;
 
     public static void play(String effect) {
-	try {
-	    URL resource = SoundEffects.class.getResource("/sounds/" + effect + ".wav");
-	    AudioInputStream input = AudioSystem.getAudioInputStream(resource);
-	    Clip clip = AudioSystem.getClip();
-	    clip.open(input);
-	    clip.start();
-	} catch (Exception e) {
-	    throw new RuntimeException(e);
-	}
+        try {
+            URL resource = SoundEffects.class.getResource("/sounds/" + effect + ".wav");
+            AudioInputStream input = AudioSystem.getAudioInputStream(resource);
+            Clip clip = AudioSystem.getClip();
+            clip.open(input);
+            clip.start();
+        } catch (UnsupportedAudioFileException e) {
+        } catch (IOException e) {
+        } catch (LineUnavailableException e) {
+        }
+    }
+
+    static class AlarmThread extends Thread {
+
+        String effect;
+
+        public AlarmThread(String eff) {
+            effect = eff;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL resource = SoundEffects.class.getResource("/sounds/" + effect + ".wav");
+                AudioInputStream input = AudioSystem.getAudioInputStream(resource);
+                Clip clip = AudioSystem.getClip();
+                clip.open(input);
+                while(alarmPlay) {
+                    clip.start();
+                    clip.loop(Clip.LOOP_CONTINUOUSLY);
+                }
+                clip.stop();
+            } catch (IOException e) {
+            } catch (LineUnavailableException e) {
+            } catch (UnsupportedAudioFileException e) {
+            }
+        }
+
+    }
+
+    public static void playAlarm(String effect) {
+        alarmPlay = true;
+        AlarmThread at = new AlarmThread(effect);
+        at.start();
     }
 
     public static void playEntry() {
-	play("start");
+        play("start");
     }
 
     public static void playCredit() {
-	play("credit");
+        play("credit");
     }
 
     public static void startAlarm() {
-	play("ghost_alarm");
+        playAlarm("ghost_alarm");
     }
-    
+
     public static void stopAlarm() {
-        if(alarm != null)
-            alarm.stop();
+        alarmPlay = false;
     }
-    
-    public static void contactDot()
-    {
-        if(!openClose)
-        {
+
+    public static void contactDot() {
+        if (!openClose) {
             playOpen();
         } else {
             playClose();
         }
         openClose = !openClose;
     }
-    
-    public static void playClose()
-    {
-	play("pacman_close");
+
+    public static void playClose() {
+        play("pacman_close");
     }
-    
+
     public static void playOpen() {
-	play("pacman_open");
+        play("pacman_open");
     }
-    
-    public static void playPacmanDead()
-    {
-	play("pac_die");
+
+    public static void playPacmanDead() {
+        play("pac_die");
     }
 }
